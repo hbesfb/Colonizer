4b2eeef67e41ae5aed53561eccf9611527e458a4
Mary Gertrude Lie-Nielsen <maryli@ihelse.net>
2025-12-05 15:51:20 +0100

    refactor: defaults to False if the attribute doesn’t exist. Only admins can access settings

--- webdaemon/routes/admin.py
+++ webdaemon/routes/admin.py
@@ -5,7 +5,8 @@ blueprint = Blueprint("admin",__name__, url_prefix="/admin")
 
 @blueprint.route('/settings', methods=['GET'])
 def admin_settings():
-	if not g.isAdmin:
+	if not getattr(g, 'isAdmin', False):
+		current_app.logger.warning("Unauthorized access attempt to /admin/settings")
 		abort(404)
 
 	return render_template('settings.html', settings=settings.data)#, form=form)
