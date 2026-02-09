274e83f306d0e6773218e1ba173a56805845067a
Mary Gertrude Lie-Nielsen <maryli@ihelse.net>
2026-02-09 11:24:32 +0100

    chore: minor edits and comments

--- webdaemon/routes/list.py
+++ webdaemon/routes/list.py
@@ -5,10 +5,12 @@ from webdaemon.database import db
 
 blueprint = Blueprint("list",__name__,url_prefix="/settleplate")
 
+# Route to list settleplates (GET) with optional filtering and deletion (POST) for admins
 @blueprint.route('/list', methods=['GET', 'POST'])
 def settleplates():
 	# if get
 	if request.method == 'POST' and getattr(g, "isAdmin", False):
+		# delete selected settleplates
 		selected = request.form.getlist("selected")
 		for settleplate_id in selected:
 			settleplate = Settleplate.query.get(int(settleplate_id))
@@ -23,7 +25,8 @@ def settleplates():
 
 	#define query
 	query = Settleplate.query
-	# filter date
+
+	# filter by date
 	try:
 		a = date(*map(int, date_from.split('-')))
 		b = date(*map(int, date_to.split('-')))
@@ -32,13 +35,15 @@ def settleplates():
 			Settleplate.ScanDate <= datetime(b.year, b.month, b.day, 23, 59, 59)
 		)
 
-	except:
+	except: # if date parsing fails, default to last 7 days
 		query = query.filter(
 			Settleplate.ScanDate >= datetime.today() - timedelta(days=7)
 		)
+
 	# filter by batch
 	if batch != "":
 		query = query.filter(Settleplate.Batch.contains(batch))
-	# return	results
+
+	# execute query and return results
 	settleplates = query.order_by(Settleplate.ScanDate.desc()).all()
 	return render_template('list.html', settleplates=settleplates, date_from=date_from, date_to=date_to, batch=batch)
