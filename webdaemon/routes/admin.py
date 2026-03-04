6d42e5a1e9691b2f101316d22d50eb05ae980999
Mary Gertrude Lie-Nielsen <maryli@ihelse.net>
2026-03-04 10:16:05 +0100

    chore: back to original

--- webdaemon/routes/admin.py
+++ webdaemon/routes/admin.py
@@ -5,8 +5,7 @@ blueprint = Blueprint("admin",__name__, url_prefix="/admin")
 
 @blueprint.route('/settings', methods=['GET'])
 def admin_settings():
-	if not getattr(g, 'isAdmin', False):
-		current_app.logger.warning("Unauthorized access attempt to /admin/settings")
+	if not g.isAdmin:
 		abort(404)
 
 	return render_template('settings.html', settings=settings.data)#, form=form)
