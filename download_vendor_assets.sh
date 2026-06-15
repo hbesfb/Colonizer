c381c3c913160faa0a3dd1f3204857c80cdf33cc
Mary Gertrude Lie-Nielsen <maryli@ihelse.net>
2026-06-15 10:39:17 +0200

    feat: download needed files since in gitlab CI sources are blocked for download

---
+++ download_vendor_assets.sh
@@ -0,0 +1,26 @@
+# FontAwesome
+mkdir -p webdaemon/static/fontawesome
+wget https://github.com/FortAwesome/Font-Awesome/releases/download/5.15.4/fontawesome-free-5.15.4-web.zip -O webdaemon/static/fontawesome/fontawesome-free-5.15.4-web.zip
+
+# Bootstrap
+mkdir -p webdaemon/static/bootstrap
+wget https://github.com/twbs/bootstrap/archive/v4.6.2.zip -O webdaemon/static/bootstrap/v4.6.2.zip
+
+# jQuery
+mkdir -p webdaemon/static/jquery
+wget https://code.jquery.com/jquery-3.7.1.min.js -O webdaemon/static/jquery/jquery.min.js
+wget https://code.jquery.com/jquery-3.7.1.js -O webdaemon/static/jquery/jquery.js
+wget https://code.jquery.com/jquery-3.7.1.min.map -O webdaemon/static/jquery/jquery.min.map
+
+# jsoneditor
+mkdir -p webdaemon/static/jsoneditor/img
+wget https://cdnjs.cloudflare.com/ajax/libs/jsoneditor/10.1.0/jsoneditor.js -O webdaemon/static/jsoneditor/jsoneditor.js
+wget https://cdnjs.cloudflare.com/ajax/libs/jsoneditor/10.1.0/jsoneditor.min.js -O webdaemon/static/jsoneditor/jsoneditor.min.js
+wget https://cdnjs.cloudflare.com/ajax/libs/jsoneditor/10.1.0/jsoneditor.css -O webdaemon/static/jsoneditor/jsoneditor.css
+wget https://cdnjs.cloudflare.com/ajax/libs/jsoneditor/10.1.0/jsoneditor.min.css -O webdaemon/static/jsoneditor/jsoneditor.min.css
+wget https://cdnjs.cloudflare.com/ajax/libs/jsoneditor/10.1.0/img/jsoneditor-icons.svg -O webdaemon/static/jsoneditor/img/jsoneditor-icons.svg
+
+# TensorFlow.js
+mkdir -p webdaemon/static/tensorflow
+wget https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.8.0/dist/tf.min.js -O webdaemon/static/tensorflow/tf.min.js
+wget https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.8.0/dist/tf.min.js.map -O webdaemon/static/tensorflow/tf.min.js.map
