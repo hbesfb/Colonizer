a10f5f42273ad46751ab31c41ecb34016819288b
Mary Gertrude Lie-Nielsen <maryli@ihelse.net>
2026-06-22 18:09:36 +0200

    feat: use tar instead of zip

--- download_vendor_assets.sh
+++ download_vendor_assets.sh
@@ -1,3 +1,8 @@
+
+# remove existing to start with fresh
+rm -rf wheeldir wheeldir.tar wheeldir.tar.part-*
+mkdir -p wheeldir
+
 # FontAwesome
 mkdir -p webdaemon/static/fontawesome
 wget https://github.com/FortAwesome/Font-Awesome/releases/download/5.15.4/fontawesome-free-5.15.4-web.zip -O webdaemon/static/fontawesome/fontawesome-free-5.15.4-web.zip
@@ -27,10 +32,34 @@ wget https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.8.0/dist/tf.min.js.map -O w
 
 # download and compress .whls files for packages in requirements_k8s.txt
 pip download --python-version 3.11 --abi cp311 --platform manylinux2014_x86_64 --only-binary=:all: \
-	-r requirements_k8s.txt -d wheeldir/ \
-	&& zip -r wheeldir.zip wheeldir/
+	-r requirements_k8s.txt -d wheeldir/ 
 
-#Split zipped dir into multiple chunks of less than 100MB and cleanup
-split -b 90M wheeldir.zip wheeldir.zip.part-
+# Create reproducible tar archive
+tar \
+  --sort=name \
+  --mtime='UTC 2020-01-01' \
+  --owner=0 \
+  --group=0 \
+  --numeric-owner \
+  -cf wheeldir.tar wheeldir
+
+#Split archive into multiple chunks of less than 100MB and cleanup
+split -b 90M wheeldir.tar wheeldir.tar.part-
+
+#cleanup
 rm -rf wheeldir/
-rm wheeldir.zip
+rm wheeldir.tar
+
+
+# rm -rf wheeldir
+# mkdir wheeldir
+
+# pip download \
+#   --python-version 3.11 \
+#   --abi cp311 \
+#   --platform manylinux2014_x86_64 \
+#   --only-binary=:all: \
+#   -r requirements_k8s.txt \
+#   -d wheeldir
+
+# find wheeldir -type f -exec sha256sum {} \; | sort > checksums2.txt
