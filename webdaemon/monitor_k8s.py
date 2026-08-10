fd4d8ff7f21eb177e05ef3c5ba09061c47bacaef
Mary Gertrude Lie-Nielsen <maryli@ihelse.net>
2026-08-10 16:24:17 +0200

    feat: save test images on Pi, have a dedicated address for checking zmq status

--- webdaemon/monitor_k8s.py
+++ webdaemon/monitor_k8s.py
@@ -15,18 +15,21 @@ class K8sServiceMonitor(threading.Thread):
     """
     Periodic service monitor used when running inside Kubernetes.
     This monitor:
-    - checks SQL, camera (via ZMQ to the Pi), and storage (PVC) every 30 seconds
+    - checks SQL, camera_zmq (via ZMQ to the Pi), and storage (PVC) every 30 seconds
+    - checks storage by asking the Pi (over ZMQ) whether its mounted storage path exists and is writable
+    - Note that camera_capture is not tracked , this avoids periodic heavy tasks on the Pi such as 
+        flashing the LEDs, capturing real images
     - avoids unnecessary checks when the UI is idle
     - runs as a dedicated background thread with a fixed interval
-    - logs transitions (eg SQL OK to FAIL, camera online to offline, storage OK to FAIL)
-    - handles offline Pi gracefully (sets camera=False and sends a log when state changes)
+    - logs transitions (eg SQL OK to FAIL, zmq and camera capture online to offline, storage OK to FAIL)
+    - handles an offline Pi gracefully (sets camera_zmq to False and logs when state changes)
     """
     def __init__(self, interval=30, sleeptimer=600):
         super().__init__(daemon=True)
         self.interval = interval
         self.sleeptimer = sleeptimer
         self._app = None
-        self._status = {"sql": False, "camera": False, "storage": False}
+        self._status = {"sql": False, "camera_zmq": False, "storage": False}
         self._lastaccess = datetime.now()
         self._lastupdate = datetime.now()
         self._lock = threading.Lock()
@@ -70,25 +73,27 @@ class K8sServiceMonitor(threading.Thread):
             log.debug(f"K8sServiceMonitor: SQL check failed: {e}")
             sql_status = False
 
-        # Check camera (ZeroMQ round‑trip from the Kubernetes pod → to the Raspberry Pi → and back)
+        # Pi online (ZMQ OK)
+        # (ZeroMQ round‑trip from the Kubernetes pod to the Raspberry Pi and back)
         # ie in k8s client.py sends JSON to the Pi (socket.send_json({"CMD": "ready"})) and expects a JSON response {"msg": True/False} from the Pi
         try:
-            camera_status = hwlayer.client.is_ready()
+            zmq_ok = hwlayer.client.is_ready()
         except Exception as e:
-            log.debug(f"K8sServiceMonitor: Camera/Pi check failed: {e}")
-            camera_status = False
+            log.debug(f"K8sServiceMonitor: Pi check failed: {e}")
+            zmq_ok = False
 
         # Check storage status - mounted path on the Pi
         storage_status = False
         try:
             storage_status = hwlayer.client.pi_mounted_storage_ok()
-        except:
+        except Exception as e:
+            log.debug(f"K8sServiceMonitor: storage check failed: {e}")
             storage_status = False
 
         # --- Update status with transition logging ---
         new_status = {
             "sql": sql_status,
-            "camera": camera_status,
+            "camera_zmq": zmq_ok,
             "storage": storage_status
         }
         with self._lock:
@@ -97,7 +102,7 @@ class K8sServiceMonitor(threading.Thread):
                 log.info(
                     f"K8sServiceMonitor: status changed | "
                     f"SQL: {old_status['sql']} to {new_status['sql']}, "
-                    f"Camera: {old_status['camera']} to {new_status['camera']}, "
+                    f"ZMQ service: {old_status['camera_zmq']} to {new_status['camera_zmq']}, "
                     f"Storage: {old_status['storage']} to {new_status['storage']}"
                 )
 
